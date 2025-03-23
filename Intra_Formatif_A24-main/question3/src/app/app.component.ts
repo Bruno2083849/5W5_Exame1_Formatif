c3ec39f680b6ebd89b9d4a678215bb3d00a49767
bruno <2083849@cegepmontpetit.ca>
2025-03-22 23:16:17 -0400

    formulaire

--- Intra_Formatif_A24-main/question3/src/app/app.component.ts
+++ Intra_Formatif_A24-main/question3/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatError, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatCard, MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
@@ -13,16 +15,81 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule]
+    imports: [MatToolbarModule, MatIconModule, MatCardModule,MatError,MatInput,MatCard,MatLabel,
+       ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule,MatInput,CommonModule]
 })
 export class AppComponent {
   title = 'reactive.form';
 
-  
+  form: FormGroup<any>;
 
-  constructor(
+  formData?: Data;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group(
+      {
+        name: ["", [Validators.required]],
+        numero:['',[Validators.required,this.adresseValidator]],
+        postalcode:['',[Validators.pattern("^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$")]],
+        comment: ["", [this.tenWorldValidator]],
+      },
+      { validators:  this.nameValidation }
+    );
+
+    // À chaque fois que les valeurs changent, notre propriété formData sera mise à jour
+    this.form.valueChanges.subscribe(() => {
+      this.formData = this.form.value;
+      
+    });
+  }
+
+  nameValidation(control: AbstractControl): ValidationErrors | null {
+    const name = control.get('name')?.value?.trim();
+    const comment = control.get('comment')?.value.trim();
+    if (!comment || !name) {
+      return null;
+    }
     
-  ) { }
+    let formValid= !comment.toLowerCase().includes(name);
+  
+    return !formValid ? { nameValidation: true } : null;
+
+  }
+
+  tenWorldValidator(control: AbstractControl): ValidationErrors | null {
+    const comment = control.value;
+    if (!comment) {
+      return null;
+    }
+    let formValid;
+    const wordCount = control.value.trim().split(/\s+/).length;
+    if(wordCount >= 10)formValid = true;
+
+    return !formValid ? { tenWorldValidator: true } : null;
+
+  }
+
+
+  adresseValidator(control: AbstractControl): ValidationErrors | null {
+    const numero = control.value;
+    if (!numero) {
+      return null;
+    }
+    let formValid;
+    if(numero >= 1000 && numero <=9999)formValid = true;
+
+    return !formValid ? { adresseValidator: true } : null;
+
+  }
+
+  
+}
+// interface qui décris le type du formulaire
+interface Data {
+  name?: string | null;
+  numero?: number|null;
+  postalcode?:string|null;
+  comment?:string|null;
+ 
 }
 
 
